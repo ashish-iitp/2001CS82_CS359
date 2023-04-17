139248414708a5396b57a50ed4f8f9feb98eb818
Ashish <ashishiitjee050602@gmail.com>
2023-04-17 23:49:06 +0530

    Tut 4 version 1

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,161 +1,144 @@
+import threading
+import numpy as np
+from queue import Queue
+import time
+from tabulate import tabulate
+import sys
+import copy
 
-from datetime import datetime
-start_time = datetime.now()
+# Global Dictionary to Map Index -> RouterName
+dicti = {}
 
-#Help https://youtu.be/H37f_x4wAC0
-def octant_longest_subsequence_count_with_range():
-    import pandas as pd
-    try:
-        data=pd.read_excel("input_octant_longest_subsequence_with_range.xlsx")
-    except FileNotFoundError:
-        print("Incorrect file name")  
-    else:
+def main(fname):
+    global dicti  # Using global dictionary
+    # Opening and reading file line by line
+    f = open(fname, "r")
+    lines = f.readlines()
+    dict = {}  # Inverse dictionary to Map RouterName -> Index
+    routerNum = int(lines.pop(0).strip())  # Getting number of routers
+    allTables = (
+        np.ones((routerNum, routerNum)) * np.inf
+    )  # Creating combined routingTable filled with inf
+    adjRouters = [
+        list() for f in range(routerNum)
+    ]  # Empty adjacentRouter table initialization
+    # Populating dictionary and reverseDictionary
+    for routerName in enumerate(lines.pop(0).strip().split(" ")):
+        dict[routerName[1]] = routerName[0]
+    dicti = {v: k for k, v in dict.items()}
+    # Populating combined routingTable and adjacentRouters
+    for line in lines:
+        if line.strip() != "EOF":
+            first, second, weight = line.strip().split(" ")
+            adjRouters[dict[first]].append(dict[second])
+            adjRouters[dict[second]].append(dict[first])
+            allTables[dict[first], dict[second]] = weight
+            allTables[dict[second], dict[first]] = weight
 
-        ##############################
-        #Creating the columns in the excel sheet
-        data["Octant"]="" 
-        data[""]=""
-        data["count"]=""
-        data["Longest Subsequence Length"]=""
-        data["Count"]=""
-        data["  "]=""
-        data["octant"]=""
-        data["Length of longest subsequence"]=""
-        data["Count of longest subsequence"]=""
-        values=["+1","-1","+2","-2","+3","-3","+4","-4"]
-        for i in range(0,8,1):
-            data.at[i,"count"]=values[i]
-        ##############################
-        lastval=data.index[-1]  #To calculate the last index, when index starts from 0 
-        ##############################################
-        #The following lines of code would check the numbers in the 3 columns and determine the octant values
-        #based on the conditional statements provided
-        #Calculating the octant value follows the same logic as described in the explanatory video
-        for i in range(0,lastval+1,1):
-            u=data["U'=U-U Avg"][i]
-            v=data["V'=V-V Avg"][i]
-            w=data["W'=W-W Avg"][i]
-            if u>0:
-                if v>0:
-                    if w>0:
-                        
-                        data.at[i,'Octant']="+1"
-                    else:
-                        
-                        data.at[i,'Octant']="-1"
-                else:
-                    if w>0:
-                        
-                        data.at[i,'Octant']="+4"
-                    else:
-                        
-                        data.at[i,'Octant']="-4"
-            else:
-                if v>0:
-                    if w>0:
-                        data.at[i,'Octant']="+2"
-                    else:
-                        data.at[i,'Octant']="-2"
-                else:
-                    if w>0:
-                        data.at[i,'Octant']="+3"
-                    else: 
-                        data.at[i,'Octant']="-3"
-        
-        
-        count=[0,0,0,0,0,0,0,0] #creating an empty array to store the counted values of consecutive occurrences
-        large=[0,0,0,0,0,0,0,0] #creating another empty array to store the largest consecutively counted occurrences
-        pos=0
-        for i in range(0,lastval,1):
-            #goes to a value at a particular time and checks if the next occurring element is same as the current one
-            if(data['Octant'][i]==data['Octant'][i+1]):     
-                for j in range(0,8,1):                      
-                    if (data['Octant'][i]==values[j]):
-                        #keeps counting till the next element is same as the previous one                              
-                        count[j]+=1
-                        pos=j
-            else:
-                #after one point the next element will not be same as the previous one
-                #in that case we'll be checking if the counted value is largest value counted yet 
-                #once the largest counted value of the octant is added to large, count is set to zero again  
-                                                        
-                if (count[pos]>large[pos]):                 
-                    large[pos]=count[pos]                   
-                    count[pos]=0      
+    # Initializing a queue and lock per node indexed using dictionary
+    # Initializing barrier to synchronize all threads
+    allQueues = []
+    locks = []
+    for i in range(0, routerNum):
+        allTables[i, i] = 0
+        allQueues.append(Queue())
+        locks.append(threading.Lock())
+    barrier = threading.Barrier(routerNum)
 
-                #in case the counted value is smaller than or equal to the previous largest counted value then 'large' remains the same as before
-                
-                else:
-                    large[pos]=large[pos]                   
-                    count[pos]=0          
-                    
-        
-        #the previous blocks counted the largest consecutive occerrence of a particular octant value
-        #but the counted value will be one less than the actual value because the initial value of 'count' was set to zero
-        #hence every octant element that exists, is given an additional count value in the code given below
-        for j in range(0,8,1):
-            for i in range(0,lastval,1):
-                if (data['Octant'][i]==values[j]):
-                    large[j]+=1
-                    break
-        
-        #code to count how many largest occurrences present for each octant value
-        count2=[0,0,0,0,0,0,0,0]              
-        for j in range(0,8,1):                   
-            for i in range(0,lastval-large[j]+1,1):
-                supercount=0
-                for k in range(0,large[j],1):
-                    if(data['Octant'][i+k]==values[j]):
-                        supercount+=1
-                if(supercount==large[j]):
-                    count2[j]+=1
-        #code to insert the final values to the excel sheet
-        for i in range(0,8,1):
-            data.at[i,"Longest Subsequence Length"]=large[i]
-            data.at[i,"Count"]=count2[i]
-        
+    # print: routers, adjacentRouters, routingTables
+    print("Routers:\t{}".format(dicti))
+    print("Adjacent routers:\t{}".format(adjRouters))
+    print("Routing Tables:\n{}".format(allTables))
+    print("Queue, Locks and Barrier Initialized")
+    print("INITIALIZATION FINISHED")
+    print("\n________________________________________________________\n")
 
-        timefromtopos=1
-        for i in range(0,8,1):
-
-            #Here we write the octant values as headings for each block in the excel sheet
-            #We also add the headings "Time", "To" and "From" to the blocks for each octant
-            data.at[timefromtopos-1,"octant"]=values[i]
-            data.at[timefromtopos,"octant"]="Time"
-            data.at[timefromtopos-1,"Length of longest subsequence"]=large[i]
-            data.at[timefromtopos,"Length of longest subsequence"]="From"
-            data.at[timefromtopos,"Count of longest subsequence"]="To"
-            data.at[timefromtopos-1,"Count of longest subsequence"]=count2[i]
-            
-            anotherpos=1
-            for j in range(0,lastval,1):
-                newcount=1
-                if(values[i]==data["Octant"][j]):   #this code checks the initial and final timestamp of the largest consecutive occerrence of an octant value
-                    for k in range(1,large[i],1):   #it goes to the octant value, checks if the next 'n' consecutive numbers are same as the current number
-                        if(large[i]<=(lastval-j)):  #where 'n' is the largest consecutive occerrence calculated previously
-                            if(data["Octant"][j]==data["Octant"][j+k]):
-                                newcount+=1
-                if(newcount==large[i]):
-                          #writes the timestamp to the excel sheet
-                          data.at[timefromtopos+anotherpos,"Length of longest subsequence"]=data["Time"][j]
-                          data.at[timefromtopos+anotherpos,"Count of longest subsequence"]=data["Time"][j+large[i]-1]
-                          anotherpos+=1  
-            timefromtopos+=2+count2[i]
-        data.to_excel("output_octant_longest_subsequence_with_range.xlsx",index=False)
+    # Initializing thread for each node
+    threads = []
+    for i in range(0, routerNum):
+        th = threading.Thread(
+            target=router,
+            args=(i, allTables[i], allQueues, adjRouters[i], locks, barrier),
+        )  # threadArguments: ( routerIdIndex, all Queues and Locks and a barrier, routingTable, adjacentRouters for current node, )
+        threads.append(th)
+        th.start()
+    # Waiting for threads to finish then join()
+    for t in threads:
+        t.join()
+    # Closing the file
+    f.close()
 
 
+#
+# Router Function: wait -> sendTable -> receiveUpdate -> printData
+#
+def router(idRouter, rTable, sharedQ, adjRouters, locks, barrier):
 
-from platform import python_version
-ver = python_version()
+    # Using global dictionary to Map Index -> RouterName
+    global dicti
+    # Initiazing indexed nextHop which will be starting node
+    nextHop = list(range(0, len(rTable)))
+    # Running Distance Vector Routing 'iterations' times
+    iterations = 4
+    for i in range(0, iterations):
+        # Waiting for 5 secs
+        time.sleep(5)
 
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+        # Acquiring lock for all adjacent nodes one by one and sending data through respective queue
+        # data = (node.routingTable, senderIndex)
+        for adj in adjRouters:
+            locks[adj].acquire()
+            dcopy = copy.deepcopy(rTable)
+            sharedQ[adj].put((dcopy, idRouter))
+            locks[adj].release()
+        # This barrier conommitted and instead locks can be used to retrieve data to maintaining concurrency and data integrity
+        # I have used barrier to simplify code execution and understanding
+        # barrier.wait()  # Waiting for everyone to send data
+
+        # Retreiving data and calculating new routing Table
+        updated = [0] * len(rTable)  # Used to check if node.routingTable gets updated
+        for adj in adjRouters:
+            newTable, senderId = sharedQ[idRouter].get(True)  # Getting data from queue
+            # Calculating new routingTable and updating 'updated'
+            costSender = rTable[senderId]
+            for j in range(0, len(rTable)):
+                #     for j in range(0, len(rTable[0])):
+                if not (rTable[j] == np.inf and newTable[j] == np.inf):
+                    if rTable[j] > newTable[j] + costSender:
+                        rTable[j] = newTable[j] + costSender
+                        nextHop[j] = senderId
+                        updated[j] = 1
+            # newTable.free()
+
+        # Wait for all threads to finish computation
+        barrier.wait()
+
+        # Priting this router's info
+        nextHopStr = [dicti[v] for v in nextHop]
+        rTableStar = []
+        for m in range(0, len(rTable)):
+            strr = str(rTable[m])
+            if updated[m] == 1:
+                strr = strr + "*"
+            rTableStar.append(strr)
+        table = [rTableStar, nextHopStr]
+        print(
+            "\n---------------{}------ITR:{}-------\n{}".format(
+                dicti[idRouter],
+                i + 1,
+                tabulate(transpose(table), headers=["Cost", "NextHop"]),
+            )
+        )
 
 
-octant_longest_subsequence_count_with_range()
+# Transposes a List
+def transpose(l1):
+    l2 = []
+    l2 = [[row[i] for row in l1] for i in range(len(l1[0]))]
+    return l2
 
-#This shall be the last lines of the code.
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
+
+# Run main()
+if __name__ == "__main__":
+
+    main(sys.argv[1])
